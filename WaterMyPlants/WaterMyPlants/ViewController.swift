5e34a077b8959fae1fe637515b0c660711fa51d0
Craig Swanson <cswanson02@me.com>
2020-02-25 23:41:56 -0600

    Created Xcode file.

---
+++ WaterMyPlants/WaterMyPlants/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WaterMyPlants
+//
+//  Created by Craig Swanson on 2/25/20.
+//  Copyright © 2020 LambdaPTBuildWeek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

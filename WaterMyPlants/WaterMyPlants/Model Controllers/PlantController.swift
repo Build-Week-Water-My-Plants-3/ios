d68753f32123cb06c3b1e65bad6df1e4d1a0b2b8
Craig Swanson <cswanson02@me.com>
2020-02-26 16:10:24 -0600

    Created model controller files.

---
+++ WaterMyPlants/WaterMyPlants/Model Controllers/PlantController.swift
@@ -0,0 +1,15 @@
+//
+//  PlantController.swift
+//  WaterMyPlants
+//
+//  Created by Craig Swanson on 2/26/20.
+//  Copyright © 2020 craigswanson. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class PlantController {
+    
+}
---
+++ WaterMyPlants/WaterMyPlants/Model Controllers/UserController.swift
@@ -0,0 +1,15 @@
+//
+//  UserController.swift
+//  WaterMyPlants
+//
+//  Created by Craig Swanson on 2/26/20.
+//  Copyright © 2020 craigswanson. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class UserController {
+    
+}

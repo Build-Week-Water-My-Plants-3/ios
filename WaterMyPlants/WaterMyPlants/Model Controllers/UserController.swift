84af5ed65b90cb2a143d72858dfca36d37e738d6
Denis Cedeno <denis.cedeno@gmail.com>
2020-02-27 00:56:14 -0500

    completed boilerplate code for user sigIn and signUp, need work on user identifier

--- WaterMyPlants/WaterMyPlants/Model Controllers/UserController.swift
+++ WaterMyPlants/WaterMyPlants/Model Controllers/UserController.swift
@@ -11,17 +11,86 @@ import UIKit
 import CoreData
 
 class UserController {
-    
     // Linking to firebase database for testing networking code
     private let baseURL = URL(string: "https://w4t3rmypl4nt5.firebaseio.com/")!
     
+    var bearer: Bearer?
+    
     // MARK: - Register New User
-    func registerUser(with user: UserRepresentation, completion: @escaping (Error?) -> Void) {
+    func signUp(with user: UserRepresentation, completion: @escaping (Error?) -> Void) {
+        // get and change endpoint!!!
+        let registerUserURL = baseURL.appendingPathComponent("Endpoint/users/signup")
+        var request = URLRequest(url: registerUserURL)
+        request.httpMethod = HTTPMethod.post.rawValue
+        // get and change endpoints!!!
+        request.setValue("application/json", forHTTPHeaderField: "content-Type")
         
+        let jsonEncoder = JSONEncoder()
+        do {
+            let jsonData = try jsonEncoder.encode(user)
+            request.httpBody = jsonData
+        } catch {
+            print("Error encoding user object: \(error)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { _, response, error in
+            if let error = error {
+                completion(error)
+                return
+            }
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
+                return
+            }
+            completion(nil)
+        }.resume()
     }
     
     // MARK: - Log In Existing User
-    func logIn(with user: UserRepresentation, completion: @escaping (Error?) -> Void) {
+    func signIn(with user: UserRepresentation, completion: @escaping (Error?) -> Void) {
+        // get and change endpoints!!!
+        let logInURL = baseURL.appendingPathComponent("users/login")
+        var request = URLRequest(url: logInURL)
+        request.httpMethod = HTTPMethod.post.rawValue
+        // get and change endpoints!!!
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         
+        let jsonEncoder = JSONEncoder()
+        do{
+            let jsonData = try jsonEncoder.encode(user)
+            request.httpBody = jsonData
+        } catch {
+            print("Error encoding user object: \(error)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(error)
+                return
+            }
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
+                return
+            }
+            guard let data = data else {
+                completion(NSError())
+                return
+            }
+            let decoder = JSONDecoder()
+            do {
+                self.bearer = try decoder.decode(Bearer.self, from: data)
+            } catch {
+                print("Erorr decoding bearer object: \(error)")
+                completion(error)
+                return
+            }
+            completion(nil)
+        } .resume()
     }
 }
--- WaterMyPlants/WaterMyPlants/View Controllers/UserSignInViewController.swift
+++ WaterMyPlants/WaterMyPlants/View Controllers/UserSignInViewController.swift
@@ -8,13 +8,98 @@
 
 import UIKit
 
-class UserSignInViewController: UIViewController {
+enum LoginType {
+    case signUp
+    case signIn
+}
 
+class UserSignInViewController: UIViewController {
+    
+    @IBOutlet private weak var usernameTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
+    @IBOutlet private weak var phoneNumberTextField: UITextField!
+    @IBOutlet private weak var loginTypeSegmentedControl: UISegmentedControl!
+    @IBOutlet private weak var signInButton: UIButton!
+    
+    var userController: UserController?
+    var loginType = LoginType.signUp
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    // MARK: - Action Handlers
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        // perform login or sign up operation based on loginType
+        guard let userController = userController else { return }
+        
+        if let username = usernameTextField.text,
+            !username.isEmpty,
+            let password = passwordTextField.text,
+            !password.isEmpty,
+            let phoneNumber = phoneNumberTextField.text,
+            !phoneNumber.isEmpty {
+            
+            let user = UserRepresentation(identifier: UUID().uuidString,
+                                          password: password,
+                                          phoneNumber: phoneNumber,
+                                          username: username)
+            
+            if loginType == .signUp {
+                userController.signUp(with: user) { error in
+                    if let error = error {
+                        print("Error occured during sign up: \(error)")
+                    } else {
+                        /// perform sign in api call that is on the main thread because its a UI call
+                        DispatchQueue.main.async {
+                            /// alert window
+                            let alertController = UIAlertController(
+                                title: "Sign Up Successfull",
+                                message: "Now please log in.",
+                                preferredStyle: .alert)
+                            /// alert button
+                            let alertAction = UIAlertAction(
+                                title: "OK",
+                                style: .default,
+                                handler: nil)
+                            /// adding action to alert controller
+                            alertController.addAction(alertAction)
+                            /// view alert controller
+                            self.present(alertController, animated: true) {
+                                self.loginType = .signIn
+                                self.loginTypeSegmentedControl.selectedSegmentIndex = 1
+                                self.signInButton.setTitle("Sign In", for: .normal)
+                            }
+                        }
+                    }
+                }
+            } else {
+                //run sign in API call
+                userController.signIn(with: user) { error in
+                    if let error = error {
+                        print("Error occurred during sign up: \(error)")
+                    } else {
+                        DispatchQueue.main.async {
+                            self.dismiss(animated: true, completion: nil)
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
+    
+    @IBAction func signInTypeChanged(_ sender: UISegmentedControl) {
+        // switch UI between login types
+        if sender.selectedSegmentIndex == 0 {
+            /// sign up mode
+            loginType = .signUp
+            signInButton.setTitle("Sign Up", for: .normal)
+        } else {
+            /// sign in mode
+            loginType = .signIn
+            signInButton.setTitle("Sign In", for: .normal)
+        }
+    }
 }
 

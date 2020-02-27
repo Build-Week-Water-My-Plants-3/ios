17ef0562fc4586ddd71535fd61d7a91d8992b545
Craig Swanson <cswanson02@me.com>
2020-02-26 23:17:22 -0600

    Completed preliminary methods for PlantController.

--- WaterMyPlants/WaterMyPlants/Model Controllers/PlantController.swift
+++ WaterMyPlants/WaterMyPlants/Model Controllers/PlantController.swift
@@ -29,10 +29,15 @@ class PlantController {
                 return
             }
 
+            // we might possibly need to make another object to post to the server depending on what the backend provides.
+            // for example, we might need to make a "plantToPostToServer" that has slightly different parameters than our current "PlantRepresentation" provides.
+
             representation.identifier = identifier.uuidString
             plant.identifier = identifier
             try CoreDataStack.shared.save()
-            request.httpBody = try JSONEncoder().encode(representation)
+            let jsonEncoder = JSONEncoder()
+            jsonEncoder.dateEncodingStrategy = .iso8601
+            request.httpBody = try jsonEncoder.encode(representation)
         } catch {
             print("Error encoding task \(error)")
             completion(error)
@@ -48,6 +53,7 @@ class PlantController {
         }.resume()
     }
 
+    // MARK: - Fetch Plant From Server
     func fetchPlantsFromServer(completion: @escaping (Error?) -> Void = {_ in }) {
         let requestURL = baseURL.appendingPathExtension("json")
 
@@ -78,7 +84,95 @@ class PlantController {
         }.resume()
     }
 
+    // MARK: - Update Core Data
+    // Assumes that the data on the server is correct and replaces core data with server data
     func updatePlants(with representations: [PlantRepresentation]) throws {
+        let plantsWithID = representations.filter { $0.identifier != nil }
+        let identifiersToFetch = plantsWithID.compactMap { $0.identifier }
         
+        let representationsByID = Dictionary(uniqueKeysWithValues: zip(identifiersToFetch, plantsWithID))
+        
+        var plantsToCreate = representationsByID
+        
+        let fetchRequest: NSFetchRequest<Plant> = Plant.fetchRequest()
+        let moc = CoreDataStack.shared.container.newBackgroundContext()
+        
+        moc.perform {
+            do {
+                let existingPlants = try moc.fetch(fetchRequest)
+                
+                for plant in existingPlants {
+                    guard let id = plant.identifier?.uuidString,
+                        let representation = representationsByID[id] else {
+                            moc.delete(plant)
+                            continue
+                    }
+                    
+                    self.update(plant: plant, representation: representation)
+                    
+                    plantsToCreate.removeValue(forKey: id)
+                }
+                
+                for representation in plantsToCreate.values {
+                    Plant(plantRepresentation: representation, context: moc)
+                }
+            } catch {
+                print("Error fetching plants for identifiers: \(error)")
+            }
+        }
+        try CoreDataStack.shared.save(context: moc)
+    }
+    
+    func update(plant: Plant, representation: PlantRepresentation) {
+        plant.identifier = UUID(uuidString: representation.identifier)
+        plant.species = representation.species
+        plant.nickname = representation.nickname
+        plant.h2oFrequency = Int32(representation.h2oFrequency)
+        plant.lastWatered = representation.lastWatered
+        plant.image = representation.image
+    }
+    
+    // MARK: - Update Existing Plant
+    // Called from detail view controller when making changes
+    func updateExistingPlant(for plant: Plant) {
+        put(plant: plant)
+        do {
+            try CoreDataStack.shared.save()
+        } catch {
+            print("Error updating existing plant")
+        }
+    }
+    
+    // MARK: - Delete Existing Plant
+    // first deletes from core data and then calls method to delete from server
+    func deletePlant(for plant: Plant) {
+        let moc = CoreDataStack.shared.mainContext
+        moc.delete(plant)
+        deletePlantFromServer(plant)
+        do {
+            try CoreDataStack.shared.save()
+        } catch {
+            print("Error deleting plant from Core Data")
+        }
+    }
+    
+    func deletePlantFromServer(_ plant: Plant, completion: @escaping (Error?) -> Void = {_ in }) {
+        guard let identifier = plant.identifier else {
+            completion(NSError())
+            return
+        }
+        
+        let requestURL = baseURL.appendingPathComponent(identifier.uuidString).appendingPathExtension("json")
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.delete.rawValue
+        
+        URLSession.shared.dataTask(with: request) { _, _, possibleError in
+            guard possibleError == nil else {
+                print("Error deleting plant from server: \(possibleError!)")
+                completion(possibleError)
+                return
+            }
+            completion(nil)
+        }.resume()
     }
 }

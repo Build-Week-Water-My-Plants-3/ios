114625565d246c863ddf83b1e8e3f1063eca714f
Denis Cedeno <denis.cedeno@gmail.com>
2020-03-04 13:08:12 -0500

    created settings view controller file and made some settings ui changes

---
+++ WaterMyPlants/WaterMyPlants/View Controllers/SettingsViewController.swift
@@ -0,0 +1,47 @@
+//
+//  SettingsViewController.swift
+//  WaterMyPlants
+//
+//  Created by denis cedeno on 3/4/20.
+//  Copyright © 2020 craigswanson. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var newPasswordTextField: UITextField!
+    
+    @IBOutlet weak var newPhoneNumberTextField: UITextField!
+    
+    @IBOutlet weak var saveButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        saveButton.layer.cornerRadius = 10
+        saveButton.layer.borderWidth = 1
+        saveButton.layer.borderColor = CGColor(srgbRed: 0.15, green: 0.30, blue: 0.75, alpha: 1.0)
+    }
+    
+
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+
+
+}
